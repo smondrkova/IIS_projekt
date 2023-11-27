98eba297fcab9d7d8874556bc039f6a5f0a36026
Barbora Smondrkova <smbaska@gmail.com>
2023-11-27 18:29:00 +0100

    moved styling

--- resources/views/event_detail.blade.php
+++ resources/views/event_detail.blade.php
@@ -11,57 +11,56 @@
 @extends('layouts.app')  
 
 @section('content')
-    <style>
-        body {
-            margin: 0;
-        }
-
-        .grid-container {
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            gap: 0px;
-            margin: auto;
-            max-width: 1000px;
-        }
-
-        .grid-item {
-            width: 100%; 
-        }
-
-        .image {
-            width: 480px;
-            height: 300px;
-        }
-
-        .text {
-            max-width: 100%;
-        }
-
-        .button-container {
-            display: flex;
-            justify-content: space-between; 
-            align-items: stretch;
-        }
-
-        .button-container a,
-        .button-container form {
-            flex: 1;
-            margin: 0 5px;
-            display: flex;
-            justify-content: center;
-        }
-
-        .button-container button {
-            flex: 1;
-        }
-
-        .links:hover {
-            color: #0056b3; 
-        }
-    </style>
-
-
     <div class="container">
+            <style>
+                body {
+                    margin: 0;
+                }
+
+                .grid-container {
+                    display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    gap: 0px;
+                    margin: auto;
+                    max-width: 1000px;
+                }
+
+                .grid-item {
+                    width: 100%; 
+                }
+
+                .image {
+                    width: 480px;
+                    height: 300px;
+                }
+
+                .text {
+                    max-width: 100%;
+                }
+
+                .button-container {
+                    display: flex;
+                    justify-content: space-between; 
+                    align-items: stretch;
+                }
+
+                .button-container a,
+                .button-container form {
+                    flex: 1;
+                    margin: 0 5px;
+                    display: flex;
+                    justify-content: center;
+                }
+
+                .button-container button {
+                    flex: 1;
+                }
+
+                .links:hover {
+                    color: #0056b3; 
+                }
+            </style>
+            
         <div class="grid-container">
             <div class="grid-item image">
                 @if($event->photo)
